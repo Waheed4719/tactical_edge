ed8decb4f3346ba2f079c55ba99c142135976e95
Waheed Uddin Ahmed <dmc4719@gmail.com>
2024-08-08 20:09:48 +0600

    Fix swagger

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -4,10 +4,6 @@ import ReactSwagger from "../components/ReactSwagger";
 export default async function IndexPage() {
   const spec = await getApiDocs();
 
-  if (!spec) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <section className="container">
       <ReactSwagger spec={spec} />
